ffd9f96e34392605bf1034f19c84ece4b92b8021
Peppe-Quint <peppequintb@gmail.com>
2020-12-15 16:09:29 +0100

    chore(*): add express sessions

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,19 @@
 const { app } = require("./../server");
+const session = require("express-session");
 
 const index = require("./index");
 const login = require("./login");
 const register = require("./register");
 
 const user = require("./user");
+
+app.use(
+  session({
+    secret: "musicbase",
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 
 app.use(user);
 
