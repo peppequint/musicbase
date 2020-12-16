761028d4fc9900b4ff9d149bcca643fd1d432dae
Peppe-Quint <peppequintb@gmail.com>
2020-12-16 09:39:16 +0100

    perf(login): redirect when user is not logged in

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 async function index(req, res) {
-  return res.render("pages/index", { data: "Dashboard", user: req.session.user });
+  if (!req.session.user) {
+    res.redirect("/login");
+  } else {
+    return res.render("pages/index", { data: "Dashboard", user: req.session.user });
+  }
 }
 
 module.exports = index;
